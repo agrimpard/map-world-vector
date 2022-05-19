4aaeed92683f48277c700cd4a625b300d909085e
agrimpard <a.grimpard@gmail.com>
2022-05-19 18:20:39 +0200

    maj

--- index.php
+++ index.php
@@ -50,7 +50,12 @@
 	var	map_zoom = 4,
 		map_zmin = 4,
 		map_zmax = 6,
-		tiles_base = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer/tile/{z}/{y}/{x}', {
+		map_center = L.latLng(49, 0),
+		map_sw = L.latLng(-89.98155760646617, -180),
+		map_ne = L.latLng(89.99346179538875, 180),
+		map_bounds = L.latLngBounds(map_sw,map_ne),
+		//tiles_base = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer/tile/{z}/{y}/{x}', {
+		tiles_base = L.tileLayer('http://localhost/dev/map-vector/arcgis-shade/{z}/{x}-{y}.jpg', {
 			minZoom: map_zmin,
 			maxZoom: map_zmax,
 			tileSize: 256,
@@ -58,10 +63,30 @@
 			noWrap: true,
 			className: 'tiles-base'
 		})
-		latlng = L.latLng(49, 0),
-		map = L.map('carte', { center: latlng, zoom: map_zoom, layers: [tiles_base], fullscreenControl: true, fullscreenControlOptions: { position: 'topleft' } } ),
+		map = L.map('carte', {
+			center: map_center,
+			maxBounds: map_bounds,
+			zoom: map_zoom,
+			layers: [tiles_base],
+			fullscreenControl: true,
+			fullscreenControlOptions: { position: 'topleft' }
+		}),
+		icon_city = L.divIcon({
+			html: '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="red" class="bi bi-record-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/></svg>',
+			className: 'icon-city',
+			iconSize: [10, 10],
+			iconAnchor:   [0, 0],
+			popupAnchor:  [0, 0]
+		}),
 		zoom_now = map.getZoom(),
-		zoom_prev = map.getZoom();
+		zoom_prev = map.getZoom(),
+		icon_country = L.icon({
+			iconUrl: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+			iconSize:     [1, 1],
+			iconAnchor:   [0, 0],
+			popupAnchor:  [0, 0]
+		});
+
 
 	// panes bathymetry
 	map.createPane('b_0'); map.getPane('b_0').style.zIndex = 501;
@@ -72,6 +97,37 @@
 	// pane countries
 	map.createPane('c_0'); map.getPane('c_0').style.zIndex = 520;
 	map.createPane('c_1'); map.getPane('c_1').style.zIndex = 521;
+	// pane labels
+	map.createPane('l_0'); map.getPane('l_0').style.zIndex = 530;
+
+	// cities
+	var cities = [
+		{
+			coords:[48.8589466,2.2769956],
+			country:'France',
+			label:'Paris',
+		},
+		{
+			coords:[52.5069704,13.2846502],
+			country:'Allemagne',
+			label:'Berlin',
+		},
+		{
+			coords:[41.9102415,12.3959152],
+			country:'Italie',
+			label:'Rome',
+		},
+		{
+			coords:[40.4381311,-3.8196195],
+			country:'Espagne',
+			label:'Madrid',
+		},
+		{
+			coords:[51.5287718,-0.2416803],
+			country:'Royaume-Uni',
+			label:'Londres',
+		},
+	];
 
 	// style
 	function geo_countries() {
@@ -146,6 +202,37 @@
 		$.getJSON('geojson/ne_10m_admin_0_boundary_lines_land.json',function(data){ geojson_frontiere = L.geoJson(data, {style: geo_frontieres, pane: 'c_0'}).addTo(map); });
 		$.getJSON('geojson/ne_10m_admin_0_countries.json',function(data){ geojson_pays = L.geoJson(data, {style: geo_countries, pane: 'c_1'}).addTo(map); });
 		L.control.scale().addTo(map);
+
+		// ville
+		var m_city = new L.Marker([48.8589466,2.2769956], { icon: icon_city, pane: 'l_0' }).bindTooltip('Paris', { pane: 'l_0', permanent: true, className: 'city-name', direction : 'top' }).addTo(map);
+
+		// pays
+		var m_country = new L.marker([46.7511877,2.4738183], { icon: icon_country, opacity: 0 }).bindTooltip('France', { pane: 'l_0', permanent: true, className: 'country-name', direction : 'center' }).addTo(map);
+/*
+			coords:[48.8589466,2.2769956],
+			country:'France',
+			label:'Paris',
+
+var marker = L.marker(props.coords, {icon: myIcon}).bindTooltip(props.country, {permanent: true, direction : 'bottom'}).addTo(mymap);
+}
+
+// Edit marker icons
+var icon_cities = L.icon({
+    iconUrl: '',
+    iconSize: [10, 10]
+});
+
+// Loop through markers
+for(var i = 0; i<markers.length; i++){
+    addMarker(markers[i]);
+}
+// To add the marker coordinates
+function addMarker(props){
+    var marker = L.marker(props.coords, {icon: icon_cities}).bindTooltip(props.country, {permanent: true, direction : 'bottom'}).addTo(map);
+}
+*/
+
+
 	}
 	map_load();
 
@@ -153,6 +240,12 @@
 		$('.geo-debug').html('<b>Zoom</b> : '+map.getZoom()+' | <b>Zoom prev</b> = '+window.zoom_prev);
 	}
 	debug_load();
+
+
+
+
+
+
 
 	map.on('zoomend', function() {
 		// zoom in / zoom out
